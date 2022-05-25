42f7369dc598f30291a8357eec6b605f694888c6
Elliot <elliotblanch@gmail.com>
2022-05-25 11:15:05 +0100

    Tests for Item & Item DAO

---
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -0,0 +1,23 @@
+package com.qa.ims.controllers;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.qa.ims.controller.ItemController;
+
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.utils.Utils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ItemControllerTest {
+
+	@Mock
+	private Utils utils;
+
+	@Mock
+	private ItemDAO dao;
+
+	@InjectMocks
+	private ItemController controller;
+}
